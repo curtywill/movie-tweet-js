896a2115d6d574a78330b519070a3b87300ffcf6
curtywill <cowilliams4@crimson.ua.edu>
2022-11-20 14:20:04 -0600

    starting point

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import {useState} from "react"
 import './App.css';
 
+const BASE_URL = "https://api.themoviedb.org/3/search/movie?api_key=337e0f9a71da9b1b8f7a3a8f96f603a0"
 function App() {
+
+  const [movieQuery, setMovieQuery] = useState("")
+  const [movies, setMovies] = useState([])
+  const handleSubmit = event => {
+    event.preventDefault() // prevents page from refreshing on submit
+
+    fetch(`${BASE_URL}&query=${encodeURIComponent(movieQuery)}`)
+    .then(response => response.json())
+    .then(response => setMovies(response.results))
+  }
+  // controlled component: input form's value is controlled by React
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Movie Tweet</h1>
+      <form onSubmit={handleSubmit}> 
+        <input 
+          value={movieQuery}
+          onChange={(event) => setMovieQuery(event.target.value)}>
+        </input>
+      </form>
+      {movies.filter(movie => movie.poster_path !== null && movie.release_date !== "")
+      .map(movie => (
+        <div>{movie.original_title}</div>
+      ))}
     </div>
-  );
+  )
 }
 
 export default App;
