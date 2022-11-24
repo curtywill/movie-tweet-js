69f9cf56ab6ff8679edfea9f0675b7d2b38f64d9
curtywill <curtywill@gmail.com>
2022-11-24 01:25:39 -0600

    sent a tweet hehe done for tn

--- server/server.js
+++ server/server.js
@@ -19,16 +19,35 @@ app.use(session({
 app.get('/oauth/twitter', async (req, res) => {
     const client = new TwitterApi({
       appKey: process.env.TWITTER_API_KEY,
-      appSecret: process.env.TWITTER_API_SECRET 
+      appSecret: process.env.TWITTER_API_SECRET
     })
     const oauthLink = await client.generateAuthLink(CALLBACK_URL)
+    if(!req.session.oauth_token) req.session.oauth_token = oauthLink.oauth_token
+    if(!req.session.oauth_token_secret) req.session.oauth_token_secret = oauthLink.oauth_token_secret
 
-    res.send(oauthLink)
-
+    res.redirect(oauthLink.url)
 })
 
-app.get('/oauth/twitter/callback', () => {
+app.get('/oauth/twitter/callback', (req, res) => {
+  const {oauth_verifier} = req.query
+  const {oauth_token, oauth_token_secret} = req.session
 
+  if (!oauth_token || !oauth_verifier || !oauth_token_secret) {
+    return res.status(400).send('You denied the app or your session expired!');
+  }
+
+  const client = new TwitterApi({
+    appKey: process.env.TWITTER_API_KEY,
+    appSecret: process.env.TWITTER_API_SECRET,
+    accessToken: oauth_token,
+    accessSecret: oauth_token_secret
+  })
+
+  client.login(oauth_verifier)
+    .then(({ client: loggedClient, accessToken, accessSecret }) => {
+      loggedClient.v1.tweet("is this working")
+    })
+    .catch(() => res.status(403).send('Invalid verifier or access tokens!'));
 })
 
 app.listen(4000, () => {
