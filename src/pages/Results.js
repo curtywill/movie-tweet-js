d1ae8a86d38e9c0ca67d5f854bdbac9ddd7ad055
curtywill <curtywill@gmail.com>
2023-01-16 19:35:38 -0600

    extra styling for results and home pages

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -18,20 +18,20 @@ export default function Results() {
   const location = useLocation()
 
   useEffect(() => {
-    if(!selectedMovie) return 
+    if (!selectedMovie) return
     if (watched) setBaseTweet(`watched ${selectedMovie.title} (${selectedMovie.release_date}) `)
     else setBaseTweet(`watching ${selectedMovie.title} (${selectedMovie.release_date}) `)
   }, [selectedMovie, watched])
 
   useEffect(() => {
-    if(location.state) setMovies(location.state.movies)
+    if (location.state) setMovies(location.state.movies)
   }, [location.state])
 
   const handleTweetSubmit = async (event) => {
     event.preventDefault()
     //sendMovieTweet(tweet, selectedMovie.poster)
     let formData = new FormData()
-    formData.append("tweet", baseTweet+comments)
+    formData.append("tweet", baseTweet + comments)
     formData.append("poster", selectedMovie.poster, "poster.jpg")
     const response = await fetch('http://localhost:4000/post/twitter', {
       method: "POST",
@@ -39,7 +39,7 @@ export default function Results() {
       credentials: 'include',
       body: formData
     })
-    if(response.ok) {
+    if (response.ok) {
       const resJson = await response.json()
       navigate('/success', {
         state: {
@@ -56,21 +56,44 @@ export default function Results() {
       <div className="grid grid-cols-2">
         {movies.map(movie => <MovieBox key={movie.id} movie={movie} updateModalState={setModalIsOpen} updateMovieState={setSelectedMovie} />)}
       </div>
-      <ReactModal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
-        <form onSubmit={handleTweetSubmit}>
-          <label htmlFor="baseTweet">Base Tweet</label>
-          <input id="baseTweet" value={baseTweet} readOnly />
-          <label htmlFor="comments">Comments (optional)</label>
-          <textarea
-            id="comments"
-            value={comments}
-            maxLength={MAX_TWEET_LENGTH - baseTweet.length}
-            onChange={(e) => setComments(e.target.value)} />
-          <input type="checkbox" id="watched" onChange={() => { setWatched(!watched) }} />
-          <label htmlFor="watched">Watched?</label>
-          <input type="submit" value="Send Tweet!" />
-        </form>
-        <p>{(baseTweet + comments).length}/{MAX_TWEET_LENGTH}</p>
+      <ReactModal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)} className="absolute top-0 bottom-0 left-0 right-0 m-auto bg-white border border-gray-200 shadow-md max-w-[50%] max-h-[60%]">
+        <div className="w-full h-full p-4 rounded-lg sm:p-6 md:p-8">
+          <form className="space-y-6" onSubmit={handleTweetSubmit}>
+            <div>
+              <label htmlFor="base-tweet" className="block mb-2 text-sm font-medium text-gray-900">Base Tweet</label>
+              <input 
+                id="base-tweet" 
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" 
+                value={baseTweet} 
+                readOnly />
+            </div>
+            <div>
+              <label htmlFor="comments" className="block mb-2 text-sm font-medium text-gray-900">Comments (optional)</label>
+              <textarea
+                rows={6}
+                id="comments"
+                value={comments}
+                onChange={(e) => setComments(e.target.value)}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full max-h-[30%] p-2.5 resize-none" />
+            </div>
+            <div className="flex items-start">
+              <div className="flex items-start">
+                <div className="flex items-center h-5">
+                  <input 
+                    id="watched" 
+                    type="checkbox"
+                    onChange={() => setWatched(!watched)} 
+                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300" />
+                </div>
+                <label htmlFor="watched" className="ml-2 text-sm font-medium">Watched?</label>
+              </div>
+              <p className="ml-auto text-sm font-medium">{(baseTweet + comments).length}/{MAX_TWEET_LENGTH}</p>
+            </div>
+            <button 
+              type="submit"
+              className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Send Tweet!</button>
+          </form>
+        </div>
       </ReactModal>
     </div>
   )
